3a4a2dbc4e2ab6256bf77750e33566d9fa2db4ef
ANKIT-stack-glitch <poudyalankit25@gmail.com>
2025-07-19 22:12:41 +0545

    Print selected values to console

--- script.js
+++ script.js
@@ -49,3 +49,16 @@ const sortInputArray = (event) => {
     inputValues.push(dropdowns[i].value);
   }
 };
+
+const sortInputArray = (event) => {
+  event.preventDefault();
+
+  const dropdowns = document.getElementsByClassName("values-dropdown");
+  const inputValues = [];
+
+  for (let i = 0; i < dropdowns.length; i++) {
+    inputValues.push(dropdowns[i].value);
+  }
+
+  console.log(inputValues);
+};
